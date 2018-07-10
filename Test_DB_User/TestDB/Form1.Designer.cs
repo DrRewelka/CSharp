738dd592ab0310e6a9b7844a2874fe3b2e2a975d
Radosław Ślipko <artem6724@gmail.com>
2018-07-10 21:40:05 +0200

    Testing Database connection with user interactions
    Program for testing database connection with user interactions, written before actual project.

---
+++ Test_DB_User/TestDB/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace TestDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.insert = new System.Windows.Forms.Button();
+            this.showAll = new System.Windows.Forms.Button();
+            this.testDBDataSet = new TestDB.TestDBDataSet();
+            this.bookBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bookTableAdapter = new TestDB.TestDBDataSetTableAdapters.BookTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookDescriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.remove = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.testDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Book description:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // insert
+            // 
+            this.insert.Location = new System.Drawing.Point(248, 4);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(75, 23);
+            this.insert.TabIndex = 4;
+            this.insert.Text = "Insert";
+            this.insert.UseVisualStyleBackColor = true;
+            this.insert.Click += new System.EventHandler(this.insert_Click_1);
+            // 
+            // showAll
+            // 
+            this.showAll.Location = new System.Drawing.Point(248, 34);
+            this.showAll.Name = "showAll";
+            this.showAll.Size = new System.Drawing.Size(75, 23);
+            this.showAll.TabIndex = 5;
+            this.showAll.Text = "Show all";
+            this.showAll.UseVisualStyleBackColor = true;
+            this.showAll.Click += new System.EventHandler(this.showAll_Click_1);
+            // 
+            // testDBDataSet
+            // 
+            this.testDBDataSet.DataSetName = "TestDBDataSet";
+            this.testDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bookBindingSource
+            // 
+            this.bookBindingSource.DataMember = "Book";
+            this.bookBindingSource.DataSource = this.testDBDataSet;
+            // 
+            // bookTableAdapter
+            // 
+            this.bookTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.bookNameDataGridViewTextBoxColumn,
+            this.bookDescriptionDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.bookBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(398, 188);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // bookNameDataGridViewTextBoxColumn
+            // 
+            this.bookNameDataGridViewTextBoxColumn.DataPropertyName = "BookName";
+            this.bookNameDataGridViewTextBoxColumn.HeaderText = "BookName";
+            this.bookNameDataGridViewTextBoxColumn.Name = "bookNameDataGridViewTextBoxColumn";
+            // 
+            // bookDescriptionDataGridViewTextBoxColumn
+            // 
+            this.bookDescriptionDataGridViewTextBoxColumn.DataPropertyName = "BookDescription";
+            this.bookDescriptionDataGridViewTextBoxColumn.HeaderText = "BookDescription";
+            this.bookDescriptionDataGridViewTextBoxColumn.Name = "bookDescriptionDataGridViewTextBoxColumn";
+            // 
+            // remove
+            // 
+            this.remove.Location = new System.Drawing.Point(389, 34);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(75, 23);
+            this.remove.TabIndex = 7;
+            this.remove.Text = "Remove";
+            this.remove.UseVisualStyleBackColor = true;
+            this.remove.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(444, 6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(63, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Record number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(431, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Login";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(434, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Password";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(490, 91);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(490, 132);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.PasswordChar = '*';
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.UseSystemPasswordChar = true;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(473, 172);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 14;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(434, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Logged in as:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(511, 237);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Guest";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(473, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Logout";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.remove);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.showAll);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.testDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private TestDBDataSet testDBDataSet;
+        private System.Windows.Forms.BindingSource bookBindingSource;
+        private TestDBDataSetTableAdapters.BookTableAdapter bookTableAdapter;
+        private System.Windows.Forms.Button showAll;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookDescriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button remove;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Test_DB_User/TestDB/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace TestDB
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection bookConn;
+        private OleDbConnection userConn;
+        private OleDbCommand oleDbCmd = new OleDbCommand();
+        private string connParam = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\TestDB.mdb";
+        private string userConnParam = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users.mdb";
+        private bool loggedIn = false;
+
+        public Form1()
+        {
+            bookConn = new OleDbConnection(connParam);
+            userConn = new OleDbConnection(userConnParam);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'testDBDataSet.Book' table. You can move, or remove it, as needed.
+            this.bookTableAdapter.Fill(this.testDBDataSet.Book);
+            label7.Text = "Guest";
+        }
+
+        private void insert_Click_1(object sender, EventArgs e)
+        {
+            bookConn.Open();
+            oleDbCmd.Connection = bookConn;
+            oleDbCmd.CommandText = "insert into Book (BookName, BookDescription) values ('" + this.textBox1.Text + "','" + this.textBox2.Text + "');";
+            oleDbCmd.ExecuteNonQuery();
+            bookConn.Close();
+
+            textBox1.Text = null;
+            textBox2.Text = null;
+
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+
+            OleDbDataAdapter dAdapter = new OleDbDataAdapter("select * from Book where ID=(select max(ID) from Book)", connParam);
+            OleDbCommandBuilder cBuilder = new OleDbCommandBuilder(dAdapter);
+
+            DataTable dataTable = new DataTable();
+            DataSet ds = new DataSet();
+
+            dAdapter.Fill(dataTable);
+
+            dataGridView1.Rows.Add(dataTable.Rows[0][0], dataTable.Rows[0][1], dataTable.Rows[0][2]);
+        }
+
+        private void showAll_Click_1(object sender, EventArgs e)
+        {
+            if (loggedIn)
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+
+                OleDbDataAdapter dAdapter = new OleDbDataAdapter("select * from Book", connParam);
+                OleDbCommandBuilder cBuilder = new OleDbCommandBuilder(dAdapter);
+
+                DataTable dataTable = new DataTable();
+                DataSet ds = new DataSet();
+
+                dAdapter.Fill(dataTable);
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    dataGridView1.Rows.Add(dataTable.Rows[i][0], dataTable.Rows[i][1], dataTable.Rows[i][2]);
+                }
+            }
+            else
+                MessageBox.Show("You are not logged in!");
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            if (loggedIn)
+            {
+                bookConn.Open();
+
+                oleDbCmd.Connection = bookConn;
+                oleDbCmd.CommandText = "delete from Book where ID = " + textBox3.Text;
+                textBox3.Text = null;
+                oleDbCmd.ExecuteNonQuery();
+
+                bookConn.Close();
+
+                dataGridView1.DataSource = null;
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+
+                OleDbDataAdapter dAdapter = new OleDbDataAdapter("select * from Book", connParam);
+                OleDbCommandBuilder cBuilder = new OleDbCommandBuilder(dAdapter);
+
+                DataTable dataTable = new DataTable();
+                DataSet ds = new DataSet();
+
+                dAdapter.Fill(dataTable);
+
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    dataGridView1.Rows.Add(dataTable.Rows[i][0], dataTable.Rows[i][1], dataTable.Rows[i][2]);
+                }
+            }
+            else
+                MessageBox.Show("You are not logged in!");
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            int temp;
+            userConn.Open();
+            oleDbCmd.Connection = userConn;
+            oleDbCmd.CommandText = "select ID from Users where Login='" + textBox4.Text + "' and ID=(select ID from Users where Password='" + textBox5.Text + "');";
+            try
+            {
+                temp = (int)oleDbCmd.ExecuteScalar();
+            }
+            catch(Exception)
+            {
+                temp = -1;
+            }
+            if (temp > 0)
+            {
+                MessageBox.Show("You are logged in!");
+                switch(temp)
+                {
+                    case 1: label7.Text = "Admin";
+                            break;
+                    case 2: label7.Text = "Kwiatkowski";
+                            break;
+                    case 3: label7.Text = "Chrumek";
+                            break;
+                }
+                textBox4.Text = null;
+                textBox5.Text = null;
+                loggedIn = true;
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password!");
+                textBox4.Text = null;
+                textBox5.Text = null;
+            }
+            userConn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (loggedIn)
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+
+                loggedIn = false;
+                label7.Text = "Guest";
+                MessageBox.Show("You have logged out!");
+            }
+        }
+    }
+}
